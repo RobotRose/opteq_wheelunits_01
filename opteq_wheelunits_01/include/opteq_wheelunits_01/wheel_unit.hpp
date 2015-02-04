12e2a10620104d05588a630ac3f2367506c44ff7
Mathijs de Langen <langen@robot-rose.nl>
2015-02-04 16:25:18 +0100

    wheel units

---
+++ opteq_wheelunits_01/include/opteq_wheelunits_01/wheel_unit.hpp
@@ -0,0 +1,136 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2013)
+*
+* Revision History:
+*	Author: Okke Hendriks
+*	Date  : 2013/12/18
+* 		- File created.
+*
+* Description:
+*	This contains the WheelUnit class, maintaining the state of the indiviual 
+* 	wheel units.
+* 
+***********************************************************************************/
+
+#ifndef WHEEL_UNIT_HPP
+#define WHEEL_UNIT_HPP
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+
+#include "ros/ros.h"
+#include "rose20_common/common.hpp"
+#include "rose20_common/ros_name.hpp"
+
+#define ROS_NAME_WU 			(ROS_NAME + "|WU")
+
+#define WHEEL_RADIUS            (0.0630)                    // [m]
+
+#define STEER_PPR	            (4096.0)                    // [#] absolute encoder ticks per rotation
+#define DRIVE_PPR            	(500.0*4.0*51.0)         	// [#] 500.0*4.0 = counts per rotation, 51.0 is gear ratio
+
+#define WHEELUNIT_MAX_ANGLE     (1024.0)                    // [pulses]
+#define WHEELUNIT_MIN_ANGLE     (-WHEELUNIT_MAX_ANGLE)      // [pulses]
+#define WHEELUNIT_MAX_VEL       (DRIVE_PPR*1.25)            // [pulses/s] Limited by encoder measure rate, higher speeds return zero velocity
+#define WHEELUNIT_MIN_VEL       (-WHEELUNIT_MAX_VEL)        // [pulses/s]
+#define STEER_VELOCITY_LIMIT    (STEER_PPR/5.0)             // [pulses/s]
+
+#define WHEELUNIT_START_MOVE_ANGLE_ERR_VAL 	((2.0*M_PI)/64.0)	// [rad] Start moving forwards again if the wheelunits are rotated to within this angle of the setpoint	
+#define WHEELUNIT_STOP_MOVE_ANGLE_ERR_VAL   ((2.0*M_PI)/24.0) 	// [rad] Stop moving forwards if the wheelunits are rotated out of this angle from the setpoint
+#define WHEELUNIT_START_MOVE_ANGLE_ERR_VAL_LOW_LEVEL ((int)(WHEELUNIT_START_MOVE_ANGLE_ERR_VAL*(STEER_PPR/(2.0*M_PI))))		// [abs encoder ticks]
+#define WHEELUNIT_STOP_MOVE_ANGLE_ERR_VAL_LOW_LEVEL	 ((int)(WHEELUNIT_STOP_MOVE_ANGLE_ERR_VAL*(STEER_PPR/(2.0*M_PI))))		// [abs encoder ticks]
+#define WHEELUNIT_STOP_TURN_SPEED_ERR_VAL   0.10 						// [m/s]
+
+using namespace std; 
+using namespace rose20_common;
+
+/**
+ * The WheelUnit class contains the functions and variables for working with the rose 2.0 swifel wheel units.
+ */
+class WheelUnit
+{
+  public:
+  	WheelUnit();
+	WheelUnit(string name, int id);
+	~WheelUnit();
+
+	// Angele setters
+	bool 	setAngleRad(float radians);
+	bool 	setAngleDeg(float degrees);
+
+	// Velocity setters
+	bool 	setVelocityRadPerSec(float rad_per_sec);
+	bool   	setVelocityMetersPerSec(float meters_per_sec);
+
+	// Angles conversion functions
+	float  	toAngleRad(int low_level_angle) const;
+	float  	toAngleRad(float low_level_angle) const;
+	float  	toAngleDeg(int low_level_angle) const;
+	float  	toAngleDeg(float low_level_angle) const;
+	int		toLowLevelSteer(float radians) const;
+	int 	toLowLevelDrive(float radians) const;
+
+	// Velocity conversion functions
+	float	toVelocityRadPerSec(int low_level_velocity) const;
+	float	toVelocityRadPerSec(float low_level_velocity) const;
+	float	toVelocityMetersPerSec(int low_level_velocity) const;
+	float	toVelocityMetersPerSec(float low_level_velocity) const;
+
+	// Angle getters
+	int		getSetAngleLowLevel() const;
+	float  	getSetAngleRad() const;
+	float 	getSetAngleDeg() const;
+	int		getMeasuredAngleLowLevel() const;
+	float 	getMeasuredAngleRad() const;
+	float 	getMeasuredAngleDeg() const;
+	int		getAngleErrorLowLevel() const;
+
+	// Velocity getters
+	int		getSetVelocityLowLevel() const;
+	float	getSetVelocityRadPerSec() const;
+	float	getSetVelocityMetersPerSec() const;
+	int		getMeasuredVelocityLowLevel() const;
+	float	getMeasuredVelocityRadPerSec() const;
+	float	getMeasuredVelocityMetersPerSec() const;
+	float	getMeasuredDiffMeters() const;
+	int		getMeasuredDiffLowLevel() const;
+	float	getDT() const;
+	int		getVelocityErrorLowLevel() const;
+
+	// IDs
+	int    	getDriveMotorID() const;
+	int    	getSteerMotorID() const;
+
+	string 		name_;       //! @todo OH: Make private what can be private?
+	int 		id_;
+	int			set_velocity_;
+	int			set_rotation_;
+	int  		encoder_measured_velocity_;
+	float 		measured_velocity_;
+	int   		measured_rotation_;	
+	int 		measured_drive_encoder_diff_;
+	float 		dT_;
+	int 		measured_drive_encoder_;			//! @todo OH: Cleanup
+	ros::Time 	measured_drive_encoder_time_;
+
+	int 		velocity_error_;		//! @todo OH: Initialize these variables 
+	int 		position_error_;
+	int 		drive_PID_out_;
+	int 		steer_PID_out_;
+	int 		drive_P_out_;
+	int 		steer_P_out_;
+	int 		drive_I_out_;
+	int 		steer_I_out_;
+	int 		drive_D_out_;
+	int 		steer_D_out_;
+	int 		MAE_;
+	int 		FE_;
+	int 		measured_drive_current_;				// [mA]
+	int 		measured_max_drive_current_;			// [mA]
+	int 		measured_steer_current_;				// [mA]
+	int 		measured_max_steer_current_;			// [mA]
+
+};
+
+#endif // WHEEL_UNIT_HPP
---
+++ opteq_wheelunits_01/src/wheel_unit.cpp
@@ -0,0 +1,262 @@
+/***********************************************************************************
+* Copyright: Rose B.V. (2013)
+*
+* Revision History:
+*	Author: Okke Hendriks
+*	Date  : 2013/12/18
+* 		- File created.
+*
+* Description:
+*	WheelUnit class
+* 
+***********************************************************************************/
+
+#include "rose20_common/wheel_unit.hpp"
+
+//! @todo OH: Move into namespace
+
+WheelUnit::WheelUnit()
+	: name_("NO NAME")
+	, id_(-1)
+	, set_rotation_(0.0)
+	, set_velocity_(0.0)
+	, measured_rotation_(0)
+    , measured_velocity_(0.0)
+    , measured_drive_encoder_(0)
+    , measured_drive_encoder_diff_(0)
+    , dT_(0)
+    , velocity_error_(0)
+	, position_error_(0)
+	, drive_PID_out_(0)
+	, steer_PID_out_(0)
+	, drive_P_out_(0)
+	, steer_P_out_(0)
+	, drive_I_out_(0)
+	, steer_I_out_(0)
+	, drive_D_out_(0)
+	, steer_D_out_(0)
+	, MAE_(0)
+	, FE_(0)
+	, measured_drive_current_(0)
+	, measured_max_drive_current_(0)
+	, measured_steer_current_(0)
+	, measured_max_steer_current_(0)
+{}
+
+WheelUnit::WheelUnit(string name, int id)
+	: name_(name)
+	, id_(id)
+	, set_rotation_(0.0)
+	, set_velocity_(0.0)
+	, measured_rotation_(0)
+    , measured_velocity_(0.0)
+    , measured_drive_encoder_(0)
+    , measured_drive_encoder_diff_(0)
+    , dT_(0)
+    , velocity_error_(0)
+	, position_error_(0)
+	, drive_PID_out_(0)
+	, steer_PID_out_(0)
+	, drive_P_out_(0)
+	, steer_P_out_(0)
+	, drive_I_out_(0)
+	, steer_I_out_(0)
+	, drive_D_out_(0)
+	, steer_D_out_(0)
+	, MAE_(0)
+	, FE_(0)
+	, measured_drive_current_(0)
+	, measured_max_drive_current_(0)
+	, measured_steer_current_(0)
+	, measured_max_steer_current_(0)
+{}
+
+WheelUnit::~WheelUnit()
+{}
+
+bool WheelUnit::setAngleRad(float radians)
+{
+	wrapToHalfPi(&radians);
+	int new_rotation = toLowLevelSteer(radians); 
+	if(limit(WHEELUNIT_MIN_ANGLE, WHEELUNIT_MAX_ANGLE, &new_rotation))
+	{
+		ROS_WARN_NAMED(ROS_NAME_WU, "Rotation limit reached, while trying to set to: %.4frad", radians);
+		return false;
+	}
+	else
+		ROS_DEBUG_NAMED(ROS_NAME_WU, "Setting Rotation: %.2frad, %d enc", radians, new_rotation);
+	
+	set_rotation_ = new_rotation;	
+	return true;	
+}
+
+bool WheelUnit::setAngleDeg(float degrees)
+{
+	return setAngleRad(degrees*(M_PI/180.0));
+}
+
+bool WheelUnit::setVelocityRadPerSec(float rad_per_sec)
+{
+	int new_velocity = toLowLevelDrive(rad_per_sec);
+	if(limit(WHEELUNIT_MIN_VEL, WHEELUNIT_MAX_VEL, &new_velocity))
+	{
+		ROS_WARN_NAMED(ROS_NAME_WU, "Rad/s limit reached, while trying to set: %.4frad/s", rad_per_sec);
+		return false;
+	}
+	else
+		ROS_DEBUG_NAMED(ROS_NAME_WU, "Setting velocity: %.2frad/s, %d pulses/s", rad_per_sec, new_velocity);
+	
+	set_velocity_ = new_velocity;
+	return true;
+}
+
+bool WheelUnit::setVelocityMetersPerSec(float meters_per_sec)
+{
+	return setVelocityRadPerSec(meters_per_sec/(WHEEL_RADIUS)); 
+}
+  
+float WheelUnit::toAngleRad(int low_level_angle) const
+{
+    return toAngleRad((float)low_level_angle);
+}
+
+float WheelUnit::toAngleRad(float low_level_angle) const
+{
+    return (low_level_angle/STEER_PPR)*M_PI*2.0;
+}
+
+int WheelUnit::toLowLevelSteer(float radians) const
+{
+    return (int)((radians*STEER_PPR)/(2.0*M_PI));
+}
+
+int WheelUnit::toLowLevelDrive(float radians) const
+{
+    return (int)((radians*DRIVE_PPR)/(2.0*M_PI));						
+}
+
+float WheelUnit::toAngleDeg(int low_level_angle) const
+{
+    return toAngleRad(low_level_angle)*(180.0/M_PI);
+}
+
+float WheelUnit::toAngleDeg(float low_level_angle) const
+{
+    return toAngleRad(low_level_angle)*(180.0/M_PI);
+}
+
+float WheelUnit::toVelocityRadPerSec(int low_level_velocity) const
+{
+	return toVelocityRadPerSec((float)low_level_velocity);
+}
+
+float WheelUnit::toVelocityRadPerSec(float low_level_velocity) const
+{
+	return ((low_level_velocity)/DRIVE_PPR)*M_PI*2.0;
+}
+
+float WheelUnit::toVelocityMetersPerSec(int low_level_velocity) const
+{
+	return toVelocityRadPerSec(low_level_velocity)*WHEEL_RADIUS;
+}
+
+float WheelUnit::toVelocityMetersPerSec(float low_level_velocity) const
+{
+	return toVelocityRadPerSec(low_level_velocity)*WHEEL_RADIUS;
+}
+
+int WheelUnit::getSetAngleLowLevel() const
+{
+    return set_rotation_;
+}
+
+float WheelUnit::getSetAngleRad() const
+{
+	return toAngleRad(set_rotation_);
+}
+
+float WheelUnit::getSetAngleDeg() const
+{
+	return toAngleDeg(set_rotation_);
+}
+
+int WheelUnit::getMeasuredAngleLowLevel() const
+{
+	return measured_rotation_;
+}
+
+float WheelUnit::getMeasuredAngleRad() const
+{
+	return toAngleRad(measured_rotation_);
+}
+
+float WheelUnit::getMeasuredAngleDeg() const
+{
+	return toAngleDeg(measured_rotation_);
+}
+
+int WheelUnit::getAngleErrorLowLevel() const
+{
+	return getSetAngleLowLevel() - getMeasuredAngleLowLevel();
+}
+
+// Returns velocity in pulses/s
+int WheelUnit::getSetVelocityLowLevel() const
+{
+	return set_velocity_;
+}
+
+float WheelUnit::getSetVelocityRadPerSec() const
+{
+	return toVelocityRadPerSec(set_velocity_);
+}
+
+float WheelUnit::getSetVelocityMetersPerSec() const
+{
+	return toVelocityMetersPerSec(set_velocity_);
+}
+
+int WheelUnit::getMeasuredVelocityLowLevel() const
+{
+	return measured_velocity_;
+}
+
+float WheelUnit::getMeasuredVelocityRadPerSec() const
+{
+	return toVelocityRadPerSec(measured_velocity_);
+}
+
+float WheelUnit::getMeasuredVelocityMetersPerSec() const
+{
+	return toVelocityMetersPerSec(measured_velocity_); 
+}
+
+float WheelUnit::getMeasuredDiffMeters() const
+{
+	return ((float)getMeasuredDiffLowLevel()/DRIVE_PPR)*M_PI*2.0*WHEEL_RADIUS;
+}
+
+int WheelUnit::getMeasuredDiffLowLevel() const
+{
+	return measured_drive_encoder_diff_;
+}
+
+float WheelUnit::getDT() const
+{
+	return dT_;
+}
+
+int WheelUnit::getVelocityErrorLowLevel() const
+{
+	return getSetVelocityLowLevel() - getMeasuredVelocityLowLevel();
+}
+
+int WheelUnit::getDriveMotorID() const
+{
+	return id_;
+}
+
+int WheelUnit::getSteerMotorID() const
+{
+	return id_ + 1;
+}
